0dd81a2b15da8a0580199c31154931c116974b49
Cam Hurley <cameron.hurley20@gmail.com>
2018-07-01 11:00:05 -0400

    commit start of js

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var wordBank = [
+    "Tom Brady", 
+    "Julian Edelman", 
+    "Chris Hogan", 
+    "Rob Gronkowski", 
+    "Donta Hightower", 
+    "Sony Michel", 
+    "James White", 
+    "Stephon Gilmore", 
+    "Patrick Chung", 
+    "Devin McCourty",
+    "Matthew Slater"
+    "Rex Burkhead"];
+
+var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+var userGuess =[
+    "a", "A",
+    "b", "B",
+    "c", "C",
+    "d", "D",
+    "e", "E",
+    "f", "F",
+    "g", "G",
+    "h", "H",
+    "i", "I",
+    "j", "J",
+    "k", "K",
+    "l", "L",
+    "m", "M",
+    "n", "N", 
+    "o", "O",
+    "p", "P",
+    "q", "Q",
+    "r", "R",
+    "s", "S",
+    "t", "T",
+    "u", "U",
+    "v", "V",
+    "w", "W", 
+    "x", "X",
+    "y", "Y",
+    "z", "Z",
+]
+
