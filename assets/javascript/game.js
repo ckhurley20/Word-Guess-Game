8a2c484c1124defac5027cd5032e88843962f192
Cam Hurley <cameron.hurley20@gmail.com>
2018-07-01 14:22:35 -0400

    fixed button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-var placeHolders = document.getElementById("#place-holders")
-var lettersGuessed = document.getElementById("#letters-guessed")
-var numWins = document.getElementById("#num-wins")
-var numLosses = document.getElementById("#num-losses")
-var guessesRemaining = document.getElementById("#guesses-remaining")
+var $placeHolders = document.getElementById("place-holders")
+var $guessedLetters = document.getElementById("guessed-letters")
+var $numWins = document.getElementById("num-wins")
+var $numLosses = document.getElementById("num-losses")
+var $guessesLeft = document.getElementById("guesses-left")
+var $newGameButton = document.getElementById("new-game-button")
 
 
 var wordBank = [
@@ -19,12 +20,23 @@ var wordBank = [
     "Matthew Slater",
     "Rex Burkhead"];
 
-var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+// var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+
 
 // console.log(randomWord);
 
 var wins = 0;
 var losses = 0;
+var guessesLeft = 8;
+var pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];;
+var pickedWordPlaceholderArr = [];
+var guessedLetterBank = [];
+var incorrectLetterBank = [];
+var gameRunning = false;
+
+pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
 
 var userChoices =[
     "a", "A",
@@ -70,4 +82,36 @@ var lettersGuessed = [];
 
     console.log(lettersGuessed);
 
-    
+
+
+// NEW GAME FUNCTION
+
+function newGame () {
+    gameRunning = true;
+    guessesLeft = 8;
+    guessedLetterBank = [];
+    incorrectLetterBank = [];
+    pickedWordPlaceholderArr = [];
+
+
+    //GENERATE RANDOM WORD
+   pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+
+    //CREATE FOR LOOP TO GIVE PICKED WORD ITS UNDERSCORES AND SPACING WHERE NECESSARY
+    for (i = 0; i < pickedWord.length; i++) {
+        if (pickedWord[i] === " ") {
+            pickedWordPlaceholderArr.push(" ");
+        } else {
+            pickedWordPlaceholderArr.push("_")
+        }
+    }
+
+    $guessesLeft.textContent = guessesLeft;
+    $placeHolders.textContent = pickedWordPlaceholderArr.join(" ");
+    $lettersGuessed.textContent = incorrectLetterBank;
+
+
+}
+
+$newGameButton.addEventListener("click", newGame);
